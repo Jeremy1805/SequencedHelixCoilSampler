5ef31933bab57c38cc5d32fb58b6106acfe3bbf5
Jeremy Guntoro <33482536+Jeremy1805@users.noreply.github.com>
2025-07-06 15:52:46 +0100

    Update ConfigurableScanner.cpp

--- src/ConfigurableScanner.cpp
+++ src/ConfigurableScanner.cpp
@@ -76,7 +76,7 @@ Config parseConfig(const std::string& filename) {
     config.y_param_range.end = y_param.at("end").get<double>();
     config.y_param_range.step = y_param.at("step").get<double>();
 
-    config.x_var_is_log = j.at("x_var_is_log").get<double>()
+    config.x_var_is_log = j.at("x_var_is_log").get<bool>()
     
     // Parse energy matrix (2D array of strings)
     auto energy_matrix_json = j.at("energy_matrix");
